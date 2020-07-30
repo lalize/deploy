2aa2903c9fbda9dc452dc01d39a703b2e07e9417
lalize <printf@kakao.com>
2020-07-30 17:11:16 +0900

    display

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import Koa, { Context } from "koa"
 import Router from "koa-router"
+import bodyParser from "koa-bodyparser"
 
 const app = new Koa()
 
@@ -9,5 +10,11 @@ router.get("/", async (ctx: Context) => {
     ctx.body = "hello"
 })
 
+router.post("/github/webhook", async (ctx: Context) => {
+    console.log(ctx.request.body)
+    ctx.body = ""
+})
+
+app.use(bodyParser())
 app.use(router.routes())
 app.listen(8080)
