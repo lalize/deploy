ef7830223236d1d4074bf5501de2811ad5b7bfda
lalize <printf@kakao.com>
2020-07-30 17:38:23 +0900

    test

--- index.ts
+++ index.ts
@@ -1,18 +1,29 @@
 import Koa, { Context } from "koa"
 import Router from "koa-router"
 import bodyParser from "koa-bodyparser"
+//import Ssh from "node-ssh"
+import fs from "fs"
+import { argv } from "yargs"
+/*
+const ssh = new Ssh()
+
+const client = ssh.connect({
+    host: <string>argv.h,
+    username: <string>argv.u,
+    port: 22,
+    privateKey: fs.readFileSync(<string>argv.i, "utf-8")
+})
+
+*/
 
 const app = new Koa()
 
 const router = new Router()
 
-router.get("/", async (ctx: Context) => {
-    ctx.body = "hello"
-})
-
-router.post("/github/webhook", async (ctx: Context) => {
-    console.log(ctx.request.body)
-    ctx.body = ""
+router.post("/github/webhook", async (ctx: Context, next: any) => {
+    console.log(ctx.request.origin)
+    console.log(ctx.request.body.repository.url)
+    next();
 })
 
 app.use(bodyParser())
